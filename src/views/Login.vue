eb7b2f7742e2a8d541e9f1d0f6f3e65e45b8a171
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-05-11 17:59:58 +0800

    add login page layout

---
+++ src/views/Login.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div class="brand">
+      <div class="brand__img">
+        <img src="../assets/img/logo@2x.png" alt="">
+      </div>
+      <div class="brand__title">
+        <h3>登入 Alphitter</h3>
+      </div>
+    </div>
+    <form action="" class="form">
+      <div class="form__group">
+        <label for="" class="second-font">
+          帳號
+        </label>
+        <input type="text" placeholder="請輸入帳號">
+      </div>
+      <div class="form__group">
+        <label for="" class="second-font">
+          密碼
+        </label>
+        <input type="password" placeholder="請輸入密碼">
+      </div>
+      <div class="form__btn">
+        <button>登入</button>
+      </div>
+    </form>
+    <div class="portal">
+      <router-link to="/register">註冊</router-link>
+      &middot;
+      <router-link to="/admin">後台登入</router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 356px;
+  padding-top: 64px;
+  margin: 0 auto;
+}
+.brand {
+  margin-bottom: 40px;
+
+  &__img {
+    margin-bottom: 24px;
+    text-align: center;
+
+    img {
+      height: 50px;
+      width: 50px;
+    }
+  }
+}
+
+.form {
+  &__group {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-bottom: 32px;
+    padding: 2px 10.55px;
+    border-radius: 2px;
+    border-bottom: 2px solid #657786;
+    height:54px;
+    background: #F5F8FA;
+
+    label {
+      color: #696974;
+      height: 26px;
+    }
+  }
+  &__btn {
+
+    button {
+      margin-top: 8px;
+      width: 356px;
+      height: 46px;
+      border-radius: 50px;
+      padding: 8px 24px;
+      background: $brand-color;
+      color: white;
+      font-size: 20px;
+      font-weight: 400;
+      line-height: 30px;
+    }
+  }
+}
+
+.portal {
+  display: flex;
+  justify-content: end;
+  margin-top: 16px;
+  width: 100%;
+  a {
+    color: $primary;
+  }
+}
+</style>
