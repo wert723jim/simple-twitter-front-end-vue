077c0b02bd309278fb6ea91dfba7bef40b39cc7d
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-05-29 15:18:03 +0800

    feat: verify access token and refresh token before router change if not valid redirect to login page

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import AdminLogin from '../views/admin/AdminLogin.vue'
 // 引用 vuex store
 import store from '../store/index'
 
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -78,9 +79,32 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  // 使用vuex actions
-  store.dispatch('fetchCurrentUser')
+router.beforeEach(async (to, from, next) => {
+  // 將 access token 取出
+  const token = localStorage.getItem('token')
+  // access token 是否有效的旗標，預設為 false(無效)/true(有效)
+  // Write-only variables are not considered as used.
+  // eslint-disable-next-line no-unused-vars
+  let accessTokenValid = false
+  // refresh token 是否有效的旗標，預設為 false(無效)/true(有效)
+  // eslint-disable-next-line no-unused-vars
+  // let refreshTokenValid = false
+  // 如果有 token 的話才驗證 access token 
+  if (token) {
+    // 使用vuex actions ，會回傳 true 
+    // 使用 async await 可以將 fetchCurrentUser return 的值取出
+    accessTokenValid = await store.dispatch('fetchCurrentUser')
+  }
+  // 如果 access token 無效的話
+  if (!accessTokenValid) {
+    // 傳送 refresh token 至後端，取得 access token 
+    accessTokenValid = await store.dispatch('getAccessToken')
+  }
+  // 若 accessTokenValid 為 false，且不是轉址到登入頁，則轉址到登入頁，若是要去登入頁則不用進入
+  if (!accessTokenValid && to.name !== 'login') {
+    next('/login')
+    return
+  }
   next()
 })
 
--- src/store/index.js
+++ src/store/index.js
@@ -37,7 +37,7 @@ export default new Vuex.Store({
   },
   actions: {
     // 在 actions 中可以透過參數方式取得 commit 的方法
-    async fetchCurrentUser({ commit, dispatch }) {
+    async fetchCurrentUser({ commit }) {
       try {
         // 呼叫 API 傳 accessToken 至後端驗證 accessToken 是否過期
         const { data } = await userAPI.getMyInfo()
@@ -49,21 +49,25 @@ export default new Vuex.Store({
           email,
           role
         })
+        return true
       } catch(err) {
         // console.log(err)
         // 若 access token 過期則傳 refreshToken 至後端取得新的 accessToken，順便驗證 refreshToken 是否過期
-        dispatch('getAccessToken')
+        // dispatch('getAccessToken')
+        return false
       } 
     },
     async getAccessToken({ commit }) {
       try {
         const { data } = await authAPI.getAccessToken()
         localStorage.setItem('token', data)
+        // 拿到 refresh token 後，重新驗證 access token
         this.dispatch('fetchCurrentUser')
+        return true
       } catch(err) {
         commit('revokeAuthentication')
-        console.log(err)
-        // 若 refreshToken 也過期，則需要重新登入，轉至登入頁
+        // 若 refreshToken 也過期，回傳 false，router 接收到後，轉至登入頁
+        return false
       }
     }
   },
