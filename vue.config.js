48b0d31887f7995f7d0f8718340ac9c5d1e61a20
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-06-13 12:23:08 +0800

    deploy setting

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
         additionalData: `@import "/src/assets/scss/_variables.scss";`
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/simple-twitter-front-end-vue/'
+  : '/'
 };
